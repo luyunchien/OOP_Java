3d5bc8085c6b589858bf9810e21730d931727257
mac <mac@macde-MacBook-Pro-2.local>
2022-05-18 14:24:26 +0100

    uploads

---
+++ cw-db/src/main/java/edu/uob/DBServer.java
@@ -0,0 +1,128 @@
+package edu.uob;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Paths;
+
+import edu.uob.DBCommands.DBCmd;
+import edu.uob.DBDataStructure.DBPath;
+import edu.uob.DBEngine.*;
+import edu.uob.DBException.DBException;
+
+/** This class implements the DB server. */
+public final class DBServer {
+
+  private static final char END_OF_TRANSMISSION = 4;
+  private final DBPath rootBase;
+  Parser parser = new Parser();
+
+  public static void main(String[] args) throws IOException {
+    new DBServer(Paths.get(".").toAbsolutePath().toFile()).blockingListenOn(8888);
+  }
+
+  /**
+   * KEEP this signature (i.e. {@code edu.uob.DBServer(File)}) otherwise we won't be able to mark
+   * your submission correctly.
+   *
+   * <p>You MUST use the supplied {@code databaseDirectory} and only create/modify files in that
+   * directory; it is an error to access files outside that directory.
+   *
+   * @param databaseDirectory The directory to use for storing any persistent database files such
+   *     that starting a new instance of the server with the same directory will restore all
+   *     databases. You may assume *exclusive* ownership of this directory for the lifetime of this
+   *     server instance.
+   */
+  public DBServer(File databaseDirectory) {
+    // TODO implement your server logic here
+    this.rootBase = new DBPath(databaseDirectory);
+  }
+
+
+  /**
+   * KEEP this signature (i.e. {@code edu.uob.DBServer.handleCommand(String)}) otherwise we won't be
+   * able to mark your submission correctly.
+   *
+   * <p>This method handles all incoming DB commands and carry out the corresponding actions.
+   */
+  public String handleCommand(String command) {
+    // TODO implement your server logic here
+    DBCmd cmd;
+    try {
+      cmd = this.parser.parse(command, rootBase);
+      cmd.interpretCMD();
+      parser.updateDatabase();
+    } catch (DBException | IOException e) {
+      e.printStackTrace();
+      return "[ERROR] There is an error message: " + e;
+    }
+    if(cmd.getCurrentTable()==null){
+      return "[OK] Thanks for your message: " + command;
+    }
+    return "[OK] Thanks for your message: " + command + cmd.convertTableToString(cmd.getCurrentTable());
+  }
+
+
+  public DBPath getRootBase(){
+    return rootBase;
+  }
+
+
+  //  === Methods below are there to facilitate server related operations. ===
+
+  /**
+   * Starts a *blocking* socket server listening for new connections. This method blocks until the
+   * current thread is interrupted.
+   *
+   * <p>This method isn't used for marking. You shouldn't have to modify this method, but you can if
+   * you want to.
+   *
+   * @param portNumber The port to listen on.
+   * @throws IOException If any IO related operation fails.
+   */
+  public void blockingListenOn(int portNumber) throws IOException {
+    try (ServerSocket s = new ServerSocket(portNumber)) {
+      System.out.println("Server listening on port " + portNumber);
+      while (!Thread.interrupted()) {
+        try {
+          blockingHandleConnection(s);
+        } catch (IOException e) {
+          System.err.println("Server encountered a non-fatal IO error:");
+          e.printStackTrace();
+          System.err.println("Continuing...");
+        }
+      }
+    }
+  }
+
+  /**
+   * Handles an incoming connection from the socket server.
+   *
+   * <p>This method isn't used for marking. You shouldn't have to modify this method, but you can if
+   * * you want to.
+   *
+   * @param serverSocket The client socket to read/write from.
+   * @throws IOException If any IO related operation fails.
+   */
+  private void blockingHandleConnection(ServerSocket serverSocket) throws IOException {
+    try (Socket s = serverSocket.accept();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()))) {
+
+      System.out.println("Connection established: " + serverSocket.getInetAddress());
+      while (!Thread.interrupted()) {
+        String incomingCommand = reader.readLine();
+        System.out.println("Received message: " + incomingCommand);
+        String result = handleCommand(incomingCommand);
+        writer.write(result);
+        writer.write("\n" + END_OF_TRANSMISSION + "\n");
+        writer.flush();
+      }
+    }
+  }
+}
